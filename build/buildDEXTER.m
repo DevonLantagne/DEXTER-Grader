5f65434b4f3387fd3692a94d310ea016f2fef807
Devon <devonlantagne@me.com>
2025-10-16 15:10:54 -0500

    Restructured repository organization
    Placed application code in src. Moved graphics and 'makefile' to build. Updated .gitignore to ignore the build artifacts but keep the makefile. Removed project file.

--- build/buildDEXTER.m
+++ build/buildDEXTER.m
@@ -1,35 +1,56 @@
 % This script builds the standalone executable.
 % You will need the MATLAB Compiler package
 
-% Build Process
+% Ensure the repo root directory is the current working directory or add
+% the repo and its subfolders to your MATLAB path.
+
+%% Build Process
+% Generates the DEXTER executable and places it in the appOuput directory
+% in build.
+
 disp("Building DEXTER Grader...")
-opts = compiler.build.StandaloneApplicationOptions("DEXTER.m");
-opts.ExecutableVersion = DEXTER.version;
+
+% Get version
+cd("src")
+dexter_version = DEXTER.version;
+cd("..")
+
+opts = compiler.build.StandaloneApplicationOptions(fullfile(".","src","DEXTER.m"));
+
+opts.ExecutableVersion = dexter_version;
 opts.Verbose = "on";
 opts.OutputDir = fullfile(".","build","appOutput");
-opts.ExecutableSplashScreen = fullfile(".","Graphics","splash.png");
-opts.ExecutableIcon = fullfile(".","Graphics","icon.png");
+opts.ExecutableSplashScreen = fullfile(".","build","graphics","splash.png");
+opts.ExecutableIcon = fullfile(".","build","graphics","icon.png");
 
-results = compiler.build.standaloneApplication(opts);
+results = compiler.build.standaloneApplication(opts); % results will be used below
+
 fprintf("\nBuild Complete!\n\n")
 
-% Package Process
+%% Package Process
+% Packages the DEXTER.exe executable into an installer. The installer is
+% saved to executables in the build folder.
+
 disp("Starting Packaging...")
+
 pack_opts = compiler.package.InstallerOptions(results); % takes build results
+
 pack_opts.ApplicationName = "DEXTER Grader";
 pack_opts.AuthorName = "Devon Lantagne";
 pack_opts.AuthorEmail = "lantagned@msoe.edu";
 pack_opts.AuthorCompany = "Milwaukee School of Engineering";
 pack_opts.Summary = "DEXTER Grader is a grading tool that gives the user more control over problems and point criteria compared to the Canvas SpeedGrader.";
 pack_opts.Description = "DEXTER Grader (DEXTER) is a grading calculator and gradebook manager. DEXTER’s main goal is to improve the efficiency and consistency of grading. The secondary goal of DEXTER is to provide insights into student performance.\n This is not a unified gradebook. DEXTER acts on individual assignments/exams; you will need a new DEXTER project per assignment. Projects are initialized using a class list and rubric file. See the User Guide on how to find, create, or format these files.\nDEXTER is a ""standalone"" MATLAB App. To use a standalone MATLAB app, you need the MATLAB Runtime. When you install DEXTER, the runtime will also be installed if it is not already on your computer. DEXTER is currently only supported on Windows OS.";
-pack_opts.Version = DEXTER.version;
-pack_opts.InstallerLogo = fullfile(".","Graphics","InstallTall.png"); % 112x290 px
-pack_opts.InstallerSplash = fullfile(".","Graphics","splash.png");
-pack_opts.InstallerIcon = fullfile(".","Graphics","icon.png"); % 48x48
-pack_opts.InstallerName = "DEXTER_installer_" + strrep(DEXTER.version,".","_");
-pack_opts.OutputDir = fullfile(".","build","installer");
+pack_opts.Version = dexter_version;
+pack_opts.InstallerLogo = fullfile(".","build","graphics","InstallTall.png"); % 112x290 px
+pack_opts.InstallerSplash = fullfile(".","build","graphics","splash.png");
+pack_opts.InstallerIcon = fullfile(".","build","graphics","icon.png"); % 48x48
+pack_opts.InstallerName = "DEXTER_installer_" + strrep(dexter_version,".","_");
+pack_opts.OutputDir = fullfile(".","build","executables");
+pack_opts.Verbose = "on";
 
-compiler.package.installer(results,'Options',pack_opts)
+compiler.package.installer(results, 'Options', pack_opts)
+
 fprintf("\nPackaging Complete!\n\n")
 
 
