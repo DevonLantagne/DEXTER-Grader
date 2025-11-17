93e0ca623384a970b8cb9629a61a495b868d40fd
Devon <devonlantagne@me.com>
2025-11-17 15:11:26 -0600

    icon pathing and startup.m still broken.

--- build/buildDEXTER.m
+++ build/buildDEXTER.m
@@ -10,10 +10,10 @@
 
 disp("Building DEXTER Grader...")
 
-% Get version
 cd("src")
+
+% Get version
 dexter_version = DEXTER.version;
-%cd("..")
 
 opts = compiler.build.StandaloneApplicationOptions(fullfile("@DEXTER","DEXTER.m"));
 
@@ -23,7 +23,7 @@ opts.OutputDir = fullfile("..","build","appOutput");
 opts.ExecutableSplashScreen = fullfile("..","build","graphics","splash.png");
 opts.ExecutableIcon = fullfile("..","build","graphics","icon.png");
 
-opts.AdditionalFiles = { fullfile("..","build","graphics","iconLarge.png") };
+opts.AdditionalFiles = { char(fullfile("..","build","graphics","iconLarge.png")) };
 
 results = compiler.build.standaloneApplication(opts); % results will be used below
 
@@ -44,15 +44,15 @@ pack_opts.AuthorCompany = "Milwaukee School of Engineering";
 pack_opts.Summary = "DEXTER Grader is a grading tool that gives the user more control over problems and point criteria compared to the Canvas SpeedGrader.";
 pack_opts.Description = "DEXTER Grader (DEXTER) is a grading calculator and gradebook manager. DEXTER’s main goal is to improve the efficiency and consistency of grading. The secondary goal of DEXTER is to provide insights into student performance. This is not a unified gradebook. DEXTER acts on individual assignments/exams; you will need a new DEXTER project per assignment. Projects are initialized using a class list and rubric file. See the DEXTER Wiki on how to find, create, or format these files. DEXTER is a ""standalone"" MATLAB App. To use a standalone MATLAB app, you need the MATLAB Runtime. When you install DEXTER, the runtime will also be installed if it is not already on your computer. DEXTER is currently only supported on Windows OS.";
 pack_opts.Version = dexter_version;
-pack_opts.InstallerLogo = fullfile(".","build","graphics","InstallTall.png"); % 112x290 px
-pack_opts.InstallerSplash = fullfile(".","build","graphics","splash.png");
-pack_opts.InstallerIcon = fullfile(".","build","graphics","icon.png"); % 48x48
+pack_opts.InstallerLogo = fullfile("..","build","graphics","InstallTall.png"); % 112x290 px
+pack_opts.InstallerSplash = fullfile("..","build","graphics","splash.png");
+pack_opts.InstallerIcon = fullfile("..","build","graphics","icon.png"); % 48x48
 pack_opts.InstallerName = "DEXTER_installer_" + strrep(dexter_version,".","_");
-pack_opts.OutputDir = fullfile(".","build","executables");
+pack_opts.OutputDir = fullfile("..","build","executables");
 pack_opts.Verbose = "on";
 
 compiler.package.installer(results, 'Options', pack_opts)
 
 fprintf("\nPackaging Complete!\n\n")
 
-
+cd("..")
